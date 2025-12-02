d619d656c69cb8e83403c844ae775bedf0b6c251
AmandaEvily <amandaelc06@gmail.com>
2025-12-02 20:01:15 +0000

    começo

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -1,3 +1,17 @@
+class Phone:
+    def __init__(self, id: str, number: str):
+        self.id = id
+        self.number = number
+
+    
+
 class Contact: 
-    def __init__(self) -> None:
-        
+    def __init__(self, name: str) -> None:
+        self.__name: str = name 
+        self.__phone = Phone
+        self.__isFavorite: bool
+
+    def getName(self):
+        return self.__name
+    
+    
